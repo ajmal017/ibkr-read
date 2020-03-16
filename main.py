5ca462824dedff77bbdd884af9f9e600dd9b5e25
Ho Chi Ho <hochi.ho@scmp.com>
2020-03-16 15:58:52 +0800

    removed comments

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import datetime
 import collections
 import inspect
 
-import logging
 import time
 import os.path
 
@@ -25,44 +24,11 @@ from ibapi.tag_value import TagValue
 
 from ibapi.contract import Contract 
 
-def SetupLogger():
-    if not os.path.exists("log"):
-        os.makedirs("log")
 
-    time.strftime("pyibapi.%Y%m%d_%H%M%S.log")
-
-    recfmt = '(%(threadName)s) %(asctime)s.%(msecs)03d %(levelname)s %(filename)s:%(lineno)d %(message)s'
-
-    timefmt = '%y%m%d_%H:%M:%S'
-
-    # logging.basicConfig( level=logging.DEBUG,
-    #                    format=recfmt, datefmt=timefmt)
-    logging.basicConfig(filename=time.strftime("log/pyibapi.%y%m%d_%H%M%S.log"),
-                        filemode="w",
-                        level=logging.INFO,
-                        format=recfmt, datefmt=timefmt)
-    logger = logging.getLogger()
-    console = logging.StreamHandler()
-    console.setLevel(logging.ERROR)
-    logger.addHandler(console)
-
-
-def printWhenExecuting(fn):
-    def fn2(self):
-        print("   doing", fn.__name__)
-        fn(self)
-        print("   done w/", fn.__name__)
-
-    return fn2
-
-
-# ! [socket_declare]
 class TestClient(EClient):
     def __init__(self, wrapper):
         EClient.__init__(self, wrapper)
-        # ! [socket_declare]
 
-        # how many times a method is called to see test coverage
         self.clntMeth2callCount = collections.defaultdict(int)
         self.clntMeth2reqIdIdx = collections.defaultdict(lambda: -1)
         self.reqId2nReq = collections.defaultdict(int)
@@ -76,41 +42,27 @@ class TestClient(EClient):
                 sign = -1 if 'cancel' in methName else 1
                 self.reqId2nReq[sign * args[idx]] += 1
             return fn(*args, **kwargs)
-
         return countReqId_
 
     def setupDetectReqId(self):
-
         methods = inspect.getmembers(EClient, inspect.isfunction)
         for (methName, meth) in methods:
             if methName != "send_msg":
-                # don't screw up the nice automated logging in the send_msg()
                 self.clntMeth2callCount[methName] = 0
-                # logging.debug("meth %s", name)
                 sig = inspect.signature(meth)
                 for (idx, pnameNparam) in enumerate(sig.parameters.items()):
                     (paramName, param) = pnameNparam # @UnusedVariable
                     if paramName == "reqId":
                         self.clntMeth2reqIdIdx[methName] = idx
-
                 setattr(TestClient, methName, self.countReqId(methName, meth))
 
-                # print("TestClient.clntMeth2reqIdIdx", self.clntMeth2reqIdIdx)
-
-
-# ! [ewrapperimpl]
 class TestWrapper(wrapper.EWrapper):
-    # ! [ewrapperimpl]
     def __init__(self):
         wrapper.EWrapper.__init__(self)
-
         self.wrapMeth2callCount = collections.defaultdict(int)
         self.wrapMeth2reqIdIdx = collections.defaultdict(lambda: -1)
         self.reqId2nAns = collections.defaultdict(int)
         self.setupDetectWrapperReqId()
-
-    # TODO: see how to factor this out !!
-
     def countWrapReqId(self, methName, fn):
         def countWrapReqId_(*args, **kwargs):
             self.wrapMeth2callCount[methName] += 1
@@ -118,37 +70,19 @@ class TestWrapper(wrapper.EWrapper):
             if idx >= 0:
                 self.reqId2nAns[args[idx]] += 1
             return fn(*args, **kwargs)
-
         return countWrapReqId_
-
     def setupDetectWrapperReqId(self):
-
         methods = inspect.getmembers(wrapper.EWrapper, inspect.isfunction)
         for (methName, meth) in methods:
             self.wrapMeth2callCount[methName] = 0
-            # logging.debug("meth %s", name)
             sig = inspect.signature(meth)
             for (idx, pnameNparam) in enumerate(sig.parameters.items()):
                 (paramName, param) = pnameNparam # @UnusedVariable
-                # we want to count the errors as 'error' not 'answer'
                 if 'error' not in methName and paramName == "reqId":
                     self.wrapMeth2reqIdIdx[methName] = idx
-
             setattr(TestWrapper, methName, self.countWrapReqId(methName, meth))
 
-            # print("TestClient.wrapMeth2reqIdIdx", self.wrapMeth2reqIdIdx)
 
-
-# this is here for documentation generation
-"""
-#! [ereader]
-        # You don't need to run this in your code!
-        self.reader = reader.EReader(self.conn, self.msg_queue)
-        self.reader.start()   # start thread
-#! [ereader]
-"""
-
-# ! [socket_init]
 class TestApp(TestWrapper, TestClient):
     def __init__(self):
         TestWrapper.__init__(self)
@@ -157,18 +91,12 @@ class TestApp(TestWrapper, TestClient):
         self.nKeybInt = 0
         self.started = False
         self.nextValidOrderId = None
-
     @iswrapper
-    # ! [nextvalidid]
     def nextValidId(self, orderId: int):
         super().nextValidId(orderId)
-
-        logging.debug("setting nextValidOrderId: %d", orderId)
         self.nextValidOrderId = orderId
         print("NextValidId:", orderId)
-    # ! [nextvalidid]
 
-        # we can start now
         self.start()
 
     def start(self):
@@ -199,59 +127,41 @@ class TestApp(TestWrapper, TestClient):
         return oid
 
     @iswrapper
-    # ! [error]
     def error(self, reqId, errorCode, errorString):
         super().error(reqId, errorCode, errorString)
         print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
 
-    # ! [error] self.reqId2nErr[reqId] += 1
-
-    @printWhenExecuting
     def historicalDataOperations_req(self):
-        # Requesting historical data
-
         contract = Contract()
         contract.symbol = "EUR"
         contract.secType = "CASH"
         contract.currency = "GBP"
         contract.exchange = "IDEALPRO"
 
+        queryTime = datetime.datetime.strptime('2020-03-11T00:00:00+0800', "%Y-%m-%dT%H:%M:%S%z").strftime("%Y%m%d %H:%M:%S")
 
-        queryTime = (datetime.datetime.today() - datetime.timedelta(days=180)).strftime("%Y%m%d %H:%M:%S")
-        self.reqHistoricalData(4102, contract, queryTime,
-                               "1 M", "1 day", "MIDPOINT", 1, 1, False, [])
+        self.reqHistoricalData(
+            4102, contract, queryTime,
+            "1 D", "30 secs", "MIDPOINT", 1, 1, False, [])
 
-    @printWhenExecuting
     def historicalDataOperations_cancel(self):
         self.cancelHistoricalData(4102)
 
     @iswrapper
-    # ! [historicaldata]
     def historicalData(self, reqId, bar):
         print("HistoricalData. ReqId:", reqId, "BarData.", bar)
-    # ! [historicaldata]
 
 def main():
-    SetupLogger()
-    logging.debug("now is %s", datetime.datetime.now())
-    logging.getLogger().setLevel(logging.ERROR)
-
     cmdLineParser = argparse.ArgumentParser("api tests")
     cmdLineParser.add_argument("-p", "--port", action="store", type=int,
                                dest="port", default=4002, help="The TCP port to use")
     args = cmdLineParser.parse_args()
     print("Using args", args)
-    logging.debug("Using args %s", args)
     app = TestApp()
-    # ! [connect]
     app.connect("127.0.0.1", args.port, clientId=0)
-    # ! [connect]
     print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
                                                     app.twsConnectionTime()))
-
-    # ! [clientrun]
     app.run()
-    # ! [clientrun]
 
 if __name__ == "__main__":
     main()
